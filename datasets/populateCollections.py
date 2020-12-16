8d4d51459092af3594c9f970d8d9b8d6943d3894
Evan <ereilly89@gmail.com>
2020-12-15 18:37:17 -0600

    added phone numbers to tenant and host sign up, added reviews, profile changes

--- datasets/populateCollections.py
+++ datasets/populateCollections.py
@@ -27,24 +27,26 @@ mycol.insert_many(list)
 
 # Generate Hosts Collection
 
-"""
+
 mycol = mydb["Host"]
 
 hosts = listing[["host_id", "host_url", "host_name", "host_since", "host_location", "host_about", "host_neighbourhood", "host_listings_count"]]
 hosts = hosts.drop_duplicates(subset=["host_id"])
-
 listDict = hosts.to_dict('records')
 list = []
+count = 0
 for key in listDict:
     key["host_id"] = str(key["host_id"])
     key["password"] = "$2b$10$0WLUV8Qq5sJgx1BZTZ/hAujp9nL0SJMRzSfqxFOEg9Pqt9cw/R7mC"
+    key["host_phone"] = 6085550000 + count
+    count = count+1
     list.append(key)
 mycol.insert_many(list)
-"""
+
 
 # Generate Reviews Collection
 
-
+"""
 reviews = pd.read_csv("reviews.csv")
 mycol = mydb["Review"]
 
@@ -56,20 +58,22 @@ for key in listDict:
     key["id"] = str(key["id"])
     list.append(key)
 mycol.insert_many(list)
-
+"""
 
 # Generate Tenants Collection
 
-"""
+
 reviews = mydb["Review"]
 list = []
+count = 0
 for x in reviews.find():
     row_dict = {}
     row_dict["tenant_id"] = str(x["reviewer_id"])
     row_dict["first"] = x["reviewer_name"]
     row_dict["last"] = ""
     row_dict["password"] = "$2b$10$0WLUV8Qq5sJgx1BZTZ/hAujp9nL0SJMRzSfqxFOEg9Pqt9cw/R7mC"
+    row_dict["tenant_phone"] = 6087560000 + count
+    count = count + 1
     list.append(row_dict)
 users = mydb["Tenant"]
 users.insert_many(list)
-"""
